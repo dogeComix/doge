d8c7e9d5cd49042b084240aa97fb33aaf5990e81
dogeComix <53877577+dogeComix@users.noreply.github.com>
2019-08-12 19:05:09 +0100

    treid to fix color changing background

--- game.js
+++ game.js
@@ -161,7 +161,13 @@ world.forever(()=>{
         levelNumber++
         level.text='level: '+levelNumber
         levelScore=1
-        var randomColor=rgba(uw.randomInt(0,225),uw.randomInt(0,225),uw.randomInt(0,225),1)
-        world.background=randomColor
+        function rainbowStop(h) 
+{
+  let f= (n,k=(n+h*12)%12) => .5-.5*Math.max(Math.min(k-3,9-k,1),-1);  
+  let rgb2hex = (r,g,b) => "#"+[r,g,b].map(x=>Math.round(x*255).toString(16).padStart(2,0)).join('');
+  return ( rgb2hex(f(0), f(8), f(4)) );
+}
+world.background = levelNumber/100
+
         }
     })
